ea2d81f2384a4887d5f567d1c0412b3e29517e4b
Wassim SEFI <wassim.sefi@esprit.tn>
2022-01-09 19:23:24 +0100

    First Commit

---
+++ lib/presentation/onboarding/onboarding.dart
@@ -0,0 +1,19 @@
+import 'package:complete_advanced_flutter/presentation/resources/color_manager.dart';
+import 'package:flutter/material.dart';
+
+class OnBoardingView extends StatefulWidget {
+  const OnBoardingView({Key? key}) : super(key: key);
+
+  @override
+  _OnBoardingViewState createState() => _OnBoardingViewState();
+}
+
+class _OnBoardingViewState extends State<OnBoardingView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+      body: Center(child: Text("Welcome to on boarding")),
+    );
+  }
+}
