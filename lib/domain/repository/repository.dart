f276113406ad7a356d9ccdd682cabe6e0159765d
Wassim SEFI <wassim.sefi@esprit.tn>
2022-01-13 11:49:24 +0100

    clean architecture part 2

--- lib/domain/repository/repository.dart
+++ lib/domain/repository/repository.dart
@@ -2,7 +2,7 @@ import 'package:complete_advanced_flutter/data/network/failure.dart';
 import 'package:complete_advanced_flutter/data/request/request.dart';
 import 'package:dartz/dartz.dart';
 
-import 'model.dart';
+import '../model/model.dart';
 
 abstract class Repository {
   Future<Either<Failure, Authentication>> login(LoginRequest loginRequest);
